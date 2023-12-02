7e2ecc4cd041f472a667c22e0286978785458e68
raralinar <raralinars@gmail.com>
2023-12-02 09:01:43 +0300

    Remove unmodified files

--- src/main/java/com/bloodbank/bloodbank/repository/ShiftDAO.java
+++
@@ -1,9 +0,0 @@
-package com.bloodbank.bloodbank.repository;
-
-import com.bloodbank.bloodbank.model.Shift;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShiftDAO extends JpaRepository<Shift, Long> {
-}
