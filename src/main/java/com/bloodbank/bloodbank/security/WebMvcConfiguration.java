61150617278cea281ae46071f10542e6a1f68cf7
raralinar <raralinars@gmail.com>
2023-12-10 08:12:21 +0300

    WebConfig for static files added

---
+++ src/main/java/com/bloodbank/bloodbank/security/WebMvcConfiguration.java
@@ -0,0 +1,17 @@
+package com.bloodbank.bloodbank.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
